d54ec95393471365b9734d851807e32d547406d6
Salattt <135066816+Salattt@users.noreply.github.com>
2024-07-26 21:43:25 +0300

    error fix

--- My project/Assets/Scripts/Sizer.cs
+++ My project/Assets/Scripts/Sizer.cs
@@ -5,39 +5,21 @@ using UnityEngine;
 
 public class Sizer : MonoBehaviour
 {
-    [SerializeField] private float _loopTime;
-    [SerializeField] private float _maxScale;
-    private float _time = 0f;
-    private float _startScale;
+    [SerializeField] private float _speed;
     private Transform _transform;
 
     public void Awake()
     {
         _transform = transform;
-        _startScale = _transform.localScale.x;
     }
 
     public void Update()
     {
-        _time += Time.deltaTime;
-
-        ChangeScale(_time);
+        ChangeScale(Time.deltaTime);
     }
 
     private void ChangeScale(float time)
     {
-        float currentLoopTime = time % _loopTime;
-        float newScale;
-
-        if (currentLoopTime < _loopTime / 2)
-        {
-            newScale = Mathf.Lerp(_startScale, _maxScale, currentLoopTime / (_loopTime / 2));
-        }
-        else
-        {
-            newScale = Mathf.Lerp(_maxScale, _startScale, currentLoopTime % ((_loopTime / 2)) / (_loopTime / 2));
-        }
-
-        _transform.localScale = new Vector3(newScale, newScale, newScale);
+        _transform.localScale += Vector3.one * _speed * time;
     }
 }
